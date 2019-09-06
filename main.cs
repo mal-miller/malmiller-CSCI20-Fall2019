f7f2ad2ab6ac1458584f8998c79fed55f78e827f
mal-miller <mal.arr.miller@gmail.com>
2019-09-06 02:37:03 +0000

    stuff

---
+++ main.cs
@@ -0,0 +1,40 @@
+using System;
+
+class MainClass {
+  public static void Main() 
+  {
+    
+    Console.WriteLine("How many cents are you depositing?");
+   
+    string TotalMoneyString;
+   
+    int Q;
+    int QM;
+    int D;
+    int DM;
+    int N;
+    int NM;
+    int P;
+    double FinalMoney;
+    int Money;
+
+    TotalMoneyString = Console.ReadLine();
+    Money = int.Parse(TotalMoneyString);
+
+    
+    Q = Money/25;
+    QM = Money%25;
+    D = QM/10;
+    DM = QM%10;
+    N = DM/5;
+    NM = DM%5;
+    P = NM/1;
+   
+    Console.WriteLine (" You deposited " +Q+ " quarter(s), " +D+ " dime(s), " +N+ " nickle(s), and " +P+ " Penny(s)");
+
+    Console.WriteLine ("But, for our services we take a fee of 10.9%");
+
+    FinalMoney = (Money - (Money*0.109)/100);
+
+    Console.WriteLine ("After my fee, you get a voucher for " +FinalMoney+ " Thank you, have a nice day.");  }
+}
